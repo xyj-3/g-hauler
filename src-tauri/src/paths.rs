b4a9f07eb8e73bde6aca9a75826636bc6b498c84
Julia Xu <41178451+xyj-3@users.noreply.github.com>
2025-04-23 18:32:57 -0400

    Add paths and structure for Windows

---
+++ src-tauri/src/paths.rs
@@ -0,0 +1,26 @@
+pub struct PlatformPath {
+    pub windows: &'static str,
+    pub macos: &'static str,
+}
+
+impl PlatformPath {
+    pub fn current(&self) -> &'static str {
+        if cfg!(target_os = "windows") {
+            self.windows
+        } else if cfg!(target_os = "macos") {
+            self.macos
+        } else {
+            panic!("Unsupported operating system: this application only supports Windows and MacOS.");
+        }
+    }
+}
+
+pub const LGHUB_DEFAULT_PATH: PlatformPath = PlatformPath {
+    windows: r"C:\Program Files\LGHUB",
+    // macos: "/Applications/LGHUB.app",
+};
+
+pub const LGHUB_DATA_PATH: PlatformPath = PlatformPath {
+    windows: r"C:\ProgramData\LGHUB",
+    // macos: "/Applications/LGHUB.app",
+};
