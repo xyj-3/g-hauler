f8cf9a53daa9a84eebba1fbeae80d55502a229ac
Julia Xu <41178451+xyj-3@users.noreply.github.com>
2025-04-29 22:23:50 -0400

    Rename to store

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,4 @@
-mod config;
+mod store;
 mod constants;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -7,8 +7,8 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            crate::config::store_get_key,
-            crate::config::store_set_key,
+            crate::store::store_get_key,
+            crate::store::store_set_key,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
