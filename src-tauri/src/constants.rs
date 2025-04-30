03fc440020ecd95b519468ea060bc07f95348039
Julia Xu <41178451+xyj-3@users.noreply.github.com>
2025-04-30 00:36:04 -0400

    Add data path constants

--- src-tauri/src/constants.rs
+++ src-tauri/src/constants.rs
@@ -1,2 +1,10 @@
 // Store constants
 pub const STORE_FILENAME: &str = "settings.json";
+
+// File paths
+pub const LGHUB_DEFAULT_PATH: &str = r"C:\Program Files\LGHUB";
+pub const LGHUB_DATA_PATH: &str = r"C:\ProgramData\LGHUB";
+pub const GAMES_DATA_PATH: &str = r"C:\Program Files\LGHUB\data\applications.json";
+pub const RELEASE_DATA_PATH: &str = r"C:\ProgramData\LGHUB\current.json";
+pub const VERSION_PATH: &str = r"C:\ProgramData\LGHUB\version.json";
+pub const POSTERS_PATH: &str = r"C:\ProgramData\LGHUB\depots\{build_id}\core_apps\images";
