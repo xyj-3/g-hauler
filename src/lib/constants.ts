93d7873b0d16b61860f404673e3a6c58a15b4994
Julia Xu <41178451+xyj-3@users.noreply.github.com>
2025-05-31 17:07:35 -0400

    Add constants.ts to access store keys in frontend

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+// A sync of src-tauri/src/constants.rs - likely a temporary solution to get constants in frontend
+
+// Store keys
+export const STORE_KEY_DATA_PATH = 'lghub_data_path';
+export const STORE_KEY_BUILD_ID = 'build_id';
+export const STORE_KEY_VERSION_NUMBER = 'version_number';
+
+// Default file paths
+export const DEFAULT_LGHUB_DATA_PATH = 'C:\\ProgramData\\LGHUB';
+export const GAMES_DATA_PATH = 'C:\\Program Files\\LGHUB\\data\\applications.json';
+export const RELEASE_DATA_PATH = 'C:\\ProgramData\\LGHUB\\current.json';
+export const VERSION_PATH = 'C:\\ProgramData\\LGHUB\\version.json';
+export const POSTERS_PATH = 'C:\\ProgramData\\LGHUB\\depots\\{build_id}\\core_apps\\images';
