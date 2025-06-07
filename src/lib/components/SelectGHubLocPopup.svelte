cf7fb9f18234438c02cde7121faee05d781b29d5
Julia Xu <41178451+xyj-3@users.noreply.github.com>
2025-06-07 14:38:17 -0600

    Add toggle for target files

--- src/lib/components/SelectGHubLocPopup.svelte
+++ src/lib/components/SelectGHubLocPopup.svelte
@@ -10,6 +10,7 @@
   export let onClose: () => void;
   let lghubPath: string = '';
   let validation: any = null;
+  let showDetails: boolean = false;
 
   onMount(async () => {
     try {
@@ -64,23 +65,28 @@
         on:click={handleBrowse}
       >Browse</button>
       {#if validation && validation.data_path_exists && validation.applications_json_exists && validation.current_json_exists && validation.version_json_exists && validation.images_dir_exists && validation.build_id !== null}
-        <span class="text-green-500 flex items-center gap-1">
+        <button
+          class="text-green-500 flex items-center gap-1"
+          on:click={() => showDetails = !showDetails}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
           </svg>
           Verified
-        </span>
+        </button>
       {:else if validation !== null}
-        <span class="text-red-500 flex items-center gap-1">
+        <button 
+          class="text-red-500 flex items-center gap-1"
+          on:click={() => showDetails = !showDetails}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
           </svg>
           Invalid
-        </span>
-      {/if}
-    </div>
-    {#if validation}
-      <div class="text-sm text-white bg-neutral-900 rounded p-2">
+        </button>
+      {/if}</div>
+    {#if validation && showDetails}
+      <div class="text-sm text-white bg-neutral-900 rounded p-2 transition-all duration-200">
         <p>Target files:</p>
         <!-- <div>Path exists: {validation.data_path_exists ? '✅' : '❌'}</div> -->
         <div>applications.json: {validation.applications_json_exists ? '✅' : '❌'}</div>
