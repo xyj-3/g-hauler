3da1a4aeb86666656d3197261fa3b3e1af44bd19
Julia Xu <41178451+xyj-3@users.noreply.github.com>
2025-06-13 18:36:37 -0600

    Add matching ts types for ghub games

---
+++ src/lib/types.ts
@@ -0,0 +1,69 @@
+// TypeScript interfaces matching the Rust structs
+
+export interface GHUBApp {
+  application_id: string;
+  category_colors: CategoryColor[];
+  commands: Command[];
+  detection: Detection[];
+  name: string;
+  poster_title_position: string;
+  poster_url: string;
+  version: number;
+}
+
+export interface CategoryColor {
+  hex: string;
+  tag: string;
+}
+
+export interface Command {
+  category: string;
+  keystroke: string[];
+  name: string;
+}
+
+export type Detection = 
+  | { steam: SteamApp }
+  | { win_registry: WinRegistry }
+  | { epic_games: EpicGames }
+  | { osx_bundle: OsxBundle }
+  | { uplay: Uplay }
+  | { gog_galaxy: GogGalaxy }
+  | { humble_app: HumbleApp }
+  | { riot_games: RiotGames }
+  | { glob: string };
+
+export interface SteamApp {
+  app_id: string;
+}
+
+export interface WinRegistry {
+  executable: string;
+  registry_key: string;
+  registry_path: string;
+}
+
+export interface EpicGames {
+  app_name: string;
+}
+
+export interface OsxBundle {
+  bundle_id: string;
+  bundle_path: string;
+}
+
+export interface Uplay {
+  app_id: string;
+}
+
+export interface GogGalaxy {
+  product_id: string;
+}
+
+export interface HumbleApp {
+  game_name: string;
+}
+
+export interface RiotGames {
+  app_name: string;
+}
