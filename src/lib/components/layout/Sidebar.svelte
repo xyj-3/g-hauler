e71ae98a0deb5cf09a0702f71caf156042a65c7d
Julia Xu <41178451+xyj-3@users.noreply.github.com>
2025-07-17 19:30:54 -0600

    Adjust icon stroke widths for Game Library and Settings in Sidebar component

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -8,7 +8,7 @@
   <div class="flex flex-col items-center gap-6 h-full w-full justify-center relative">
     <!-- Game Library -->
     <a href="/" class="w-8 h-8 flex items-center justify-center relative group">
-      <Gamepad2 class={`w-7 h-7 cursor-pointer stroke-[1.5] ${currentPath === '/' ? 'text-teal' : 'text-gray-400 hover:text-teal transition-colors duration-200'}`} />
+      <Gamepad2 class={`w-7 h-7 cursor-pointer stroke-[1.45] ${currentPath === '/' ? 'text-teal' : 'text-gray-400 hover:text-teal transition-colors duration-200'}`} />
       <!-- Hover label -->
       <div class="absolute left-full top-0 h-8 w-16 bg-gray-950 text-white text-sm flex items-center justify-center ml-3 rounded border border-gray-600 opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out transform translate-x-[-10px] group-hover:translate-x-0 pointer-events-none whitespace-nowrap z-10 before:content-[''] before:absolute before:right-full before:top-1/2 before:transform before:-translate-y-1/2 before:border-[5px] before:border-transparent before:border-r-gray-600 after:content-[''] after:absolute after:right-full after:top-1/2 after:transform after:-translate-y-1/2 after:border-[4px] after:border-transparent after:border-r-gray-950 after:translate-x-[1px]">
         Library
@@ -32,7 +32,7 @@
     </a>
     <!-- Settings -->
     <a href="/settings" class="w-8 h-8 flex items-center justify-center relative group">
-      <Settings class={`w-6 h-6 cursor-pointer stroke-[1.7] ${currentPath === '/settings' ? 'text-teal' : 'text-gray-400 hover:text-teal-400 transition-colors duration-200'}`} />
+      <Settings class={`w-6 h-6 cursor-pointer stroke-[1.65] ${currentPath === '/settings' ? 'text-teal' : 'text-gray-400 hover:text-teal-400 transition-colors duration-200'}`} />
       <!-- Hover label -->
       <div class="absolute left-full top-0 h-8 w-17 bg-gray-950 text-white text-sm flex items-center justify-center ml-3 rounded border border-gray-600 opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out transform translate-x-[-10px] group-hover:translate-x-0 pointer-events-none whitespace-nowrap z-10 before:content-[''] before:absolute before:right-full before:top-1/2 before:transform before:-translate-y-1/2 before:border-[5px] before:border-transparent before:border-r-gray-600 after:content-[''] after:absolute after:right-full after:top-1/2 after:transform after:-translate-y-1/2 after:border-[4px] after:border-transparent after:border-r-gray-950 after:translate-x-[1px]">
         Settings
