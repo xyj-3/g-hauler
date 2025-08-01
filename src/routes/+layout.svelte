c65ae1fe5f87baf4d267b59b9c90301d8120cb42
Julia Xu <41178451+xyj-3@users.noreply.github.com>
2025-08-01 16:56:37 -0600

    fix: rename and fix issues with test-build GitHub Action

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import '../app.css';
   import { onMount } from 'svelte';
   import { fade } from 'svelte/transition';
   import SplashScreen from '$components/SplashScreen.svelte';
