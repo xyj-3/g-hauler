611f46167aaa9265071f2a4cdd1ca35b70542c9a
GVwC <gytsvir@gmail.com>
2025-08-01 18:59:51 -0600

    Add autostart status to settings.json store and synced with plugin

--- src-tauri/src/constants.rs
+++ src-tauri/src/constants.rs
@@ -3,6 +3,7 @@ pub const STORE_FILENAME: &str = "settings.json";
 
 // Store keys
 pub const STORE_KEY_DATA_PATH: &str = "lghub_data_path";
+pub const STORE_KEY_AUTOSTART: &str = "autostart";
 
 // File paths
 pub const LGHUB_DEFAULT_DATA_PATH: &str = r"C:\ProgramData\LGHUB";
